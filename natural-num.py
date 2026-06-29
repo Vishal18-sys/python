531a75e3bfde018f02b106e2f48db273c89dc51e
Vishal18-sys <vy1043304@gmail.com>
2026-06-29 15:09:43 +0530

    Add new  program

--- natural-num.py
+++ natural-num.py
@@ -4,3 +4,10 @@ num=int(input("enter number: "))
 for i in range(1,num+1):
     print(i)
 print("end")
+
+# Write a program to print all natural numbers in reverse
+
+num=int(input("enter number: "))
+for i in range(num,0,-1):
+    print(i)
+print("end")
