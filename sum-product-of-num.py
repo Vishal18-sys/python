bc9243628ebdfdccf1017e153d99ce3355408e09
Vishal18-sys <vy1043304@gmail.com>
2026-06-30 13:52:41 +0530

    Sum and product of Numbers

---
+++ sum-product-of-num.py
@@ -0,0 +1,17 @@
+# write a program to Sum of numbers from 1 to n
+
+sum=0
+num=int(input('enter number: '))
+for i in range(1,num+1):
+    sum=sum+i
+print("Sum of numbers: ",sum)
+
+
+
+# write a program to Product of numbers from 1 to n
+
+product=1
+num=int(input('enter number: '))
+for i in range(1,num+1):
+    product=product*i
+print("Product of numbers:",product)
