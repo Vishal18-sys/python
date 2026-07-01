e168d95a581a60d24be4c8dfda2deafbdaaff3a9
Vishal18-sys <vy1043304@gmail.com>
2026-07-01 16:20:03 +0530

    Check no. is Positive , Negative or Zero

---
+++ positive-negative-num.py
@@ -0,0 +1,9 @@
+# Write a program to check if number is Positive , Negative or Zero
+
+num=int(input("Enter number: "))
+if num>0:
+    print("Positive number",num)
+elif num==0:
+    print("Number is zero:",num)
+else:
+    print("Number is negative:",num)
