7d38d93dd72ad052dd0f9656a666e50473eab840
Vishal18-sys <vy1043304@gmail.com>
2026-07-01 16:12:36 +0530

    Area of Square and Triangle

---
+++ area.py
@@ -0,0 +1,14 @@
+# Write a program to print the area of triangle
+
+height = float(input("Enter the height value: "))
+base = float(input("Enter the base value: "))
+area = height* base*0.5
+print("Area of Triangle is:",area)
+
+
+
+# Write a program to print the area of square
+
+side = int(input("Enter the side value: "))
+area = side*side
+print("Area of square is:",area)
