9bdb491c54e277a4b348a332992c7166055bd91a
Vishal18-sys <vy1043304@gmail.com>
2026-07-01 17:46:34 +0530

    Swap using third variable

---
+++ Swap-using-third-var.py
@@ -0,0 +1,9 @@
+# Write a program to swap number using third variable
+
+a=input("Enter a number: ")
+b=input("Enter a number: ")
+print(f"Original value of a = {a} and b = {b}")
+temp=a
+a=b
+b=temp
+print(f" Swap the value of a= {a} and b= {b}")
