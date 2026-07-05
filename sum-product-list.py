575115d63ccc0d4ebebe700d134d7a51f81091a4
Vishal18-sys <vy1043304@gmail.com>
2026-07-06 01:51:41 +0530

    Sum or Product in List

---
+++ sum-product-list.py
@@ -0,0 +1,14 @@
+# Find the sum of all elements 
+sum=0
+num=[1,2,3,4,5,6,7,8,9,10]
+for i in num:
+    sum = sum + i
+print(sum)
+
+# Product of all elements
+
+product=1
+num=[1,2,3,4,5,6,7,8,9,10]
+for i in num:
+    product = product*i
+print(product)
