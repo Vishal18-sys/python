5a613c42281eff74f2cee3155c9bab29ccc9c488
Vishal18-sys <vy1043304@gmail.com>
2026-07-05 23:45:25 +0530

    Create list and also take value user

---
+++ create-list.py
@@ -0,0 +1,15 @@
+# Create a list of 5 numbers and print it.
+
+# Give input
+
+num=[10,20,30,40,50]
+print(num)
+print(len(num))
+
+
+# Take use value 
+num=[]
+for i in range(5):
+    number=int(input('enter a number: '))
+    num.append(number)
+print(num)
