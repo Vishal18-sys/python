7326de7a4b0d3907f6b524d027be211f150fd11c
Vishal18-sys <vy1043304@gmail.com>
2026-07-06 01:24:49 +0530

    Even and Odd Number in List

---
+++ even-odd-list.py
@@ -0,0 +1,13 @@
+# Print all even numbers from a list.
+
+num=[2,3,4,5,6,7,8,9,10]
+for i in num:
+    if i%2==0:
+        print(i)
+
+# Print all even numbers from a list
+
+num=[2,3,4,5,6,7,8,9,10,11,12]
+for i in num:
+    if i%2!=0:
+        print(i)
