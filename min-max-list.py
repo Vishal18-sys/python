f48e282266f252f24a509fc81a9515d7e6cc2e09
Vishal18-sys <vy1043304@gmail.com>
2026-07-06 01:44:10 +0530

    Max num and Min num in List

---
+++ min-max-list.py
@@ -0,0 +1,12 @@
+# Find the largest element in a list using max() 
+
+num=[20,15,30,55,70,66]
+short=max(num)
+print(short)
+
+# Find the smallest element in a list using min() 
+
+
+num=[20,15,30,55,70,66]
+short1=min(num)
+print(short1)
