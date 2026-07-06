bf12462aa64417d43839a5fb379e1d1c617bfc4c
Vishal18-sys <vy1043304@gmail.com>
2026-07-07 01:37:00 +0530

    Sort a list in asc or desc order

---
+++ sort-list.py
@@ -0,0 +1,12 @@
+    # Sort a list in ascending order
+
+num=[1,3,2,5,6,4,7,9,8,0,11,15,10]
+num.sort()
+print(num)
+
+
+    # Sort a list in descending order
+
+num=[1,3,2,5,6,4,7,9,8,0,11,15,10]
+num.sort(reverse=True)
+print(num)
