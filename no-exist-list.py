925df7e6df9581c6fec782f0eb77286fb4427998
Vishal18-sys <vy1043304@gmail.com>
2026-07-06 18:39:30 +0530

    Number exist or not

---
+++ no-exist-list.py
@@ -0,0 +1,8 @@
+# Check whether a number exists in a list. 
+
+number=[10,20,30,40,50,60,70,80,90]
+num=int(input("Enter a number: "))
+if num in number:
+    print("Number is found")
+else:
+    print("Number is not found")
