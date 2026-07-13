cb8ee66fde73a60ab7540995ae41d105c0962570
Vishal18-sys <vy1043304@gmail.com>
2026-07-13 15:39:26 +0530

    Sort Dictionary in kays and also reverse order

---
+++ sort.py
@@ -0,0 +1,9 @@
+    # Sort Dictionary by Keys
+data = {"banana": 3, "apple": 5, "cherry": 1, "date": 4}
+result = dict(sorted(data.items()))
+print(result)
+
+    # Sort Dictionary by Keys in reverse order
+data = {"banana": 3, "apple": 5, "cherry": 1, "date": 4}
+result= dict(sorted(data.items(), reverse=True))
+print(result) 
